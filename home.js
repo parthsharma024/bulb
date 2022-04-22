4c3242d1303c9679af9e69f0d98bdd57a596174b
parthsharma024 <75871312+parthsharma024@users.noreply.github.com>
2022-04-22 12:53:50 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,5 @@
+// console.log("hello");
+// alert(" hii this is parth")
+//  var a=52;
+//  console.log(a);
+//  document.getElementById('first').innerHTML="Hii all This is PARTH A professionaljavascript developer"
